8ba6a086ce1ec2b2e6c6928ee115a3a9477901e6
KateSproge <148889767+KateSproge@users.noreply.github.com>
2025-02-13 13:55:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,10 +23,18 @@
         <label>Jūsu vecums:</label><br>
         <input type="text" name="vecums" required><br><br>
 
+        <label>Vai zināt, kas ir sīkdatnes un kā tās darbojas tīmekļa vietnēs?</label><br>
+        <input type="radio" name="izpratne" value="Jā" required> Jā<br>
+        <input type="radio" name="izpratne" value="Nē"> Nē<br><br>
+
         <label>Kā Jūs vērtējat sīkdatņu izmantošanu tīmekļa vietnēs?</label><br>
         <input type="radio" name="vertesana" value="Pozitīvi" required> Pozitīvi<br>
         <input type="radio" name="vertesana" value="Neitrāli"> Neitrāli<br>
         <input type="radio" name="vertesana" value="Negatīvi"> Negatīvi<br><br>
+
+        <label>Vai jums šķiet, ka izsekošana tīmekļa vietnēs ir pieņemama?</label><br>
+        <input type="radio" name="attieksme" value="Pieņemama" required> Pieņemama<br>
+        <input type="radio" name="attieksme" value="Nepieņemama"> Nepieņemama<br><br>
 
         <label>Vai Jūs uztraucaties par privātumu, ko ietekmē izsekošanas tehnoloģijas?</label><br>
         <input type="radio" name="privatums" value="Jā" required> Jā<br>
@@ -39,6 +47,26 @@
         <input type="checkbox" name="noluki[]" value="Analītika"> Analītika<br>
         <input type="checkbox" name="noluki[]" value="Cits"> Cits<br><br>
 
+        <label>Vai jums ir svarīgi, lai jūs varētu kontrolēt, kādas sīkdatnes tiek izmantotas?</label><br>
+        <input type="radio" name="kontrole" value="Jā" required> Jā<br>
+        <input type="radio" name="kontrole" value="Nē"> Nē<br><br>
+
+        <label>Vai esat informēti par to, kādas informācijas tiek vāktas, izmantojot sīkdatnes?</label><br>
+        <input type="radio" name="informacija" value="Jā" required> Jā<br>
+        <input type="radio" name="informacija" value="Nē"> Nē<br><br>
+
+        <label>Vai jums šķiet, ka uzņēmumiem būtu jāsniedz vairāk informācijas par to, kā tiek izmantotas jūsu dati?</label><br>
+        <input type="radio" name="informacija_uznemumi" value="Jā" required> Jā<br>
+        <input type="radio" name="informacija_uznemumi" value="Nē"> Nē<br><br>
+
+        <label>Vai jūs izmantojat iespējas mainīt sīkdatņu preferences, kad apmeklējat tīmekļa vietnes?</label><br>
+        <input type="radio" name="preferences" value="Jā" required> Jā<br>
+        <input type="radio" name="preferences" value="Nē"> Nē<br><br>
+
+        <label>Vai jūs lasāt privātuma politiku vai sīkdatņu izmantošanas nosacījumus, kad apmeklējat tīmekļa vietnes?</label><br>
+        <input type="radio" name="lasisana" value="Jā" required> Jā<br>
+        <input type="radio" name="lasisana" value="Nē"> Nē<br><br>
+        
         <label>Jūsu komentāri vai ieteikumi:</label><br>
         <textarea name="komentari" rows="4" cols="50"></textarea><br><br>
 
